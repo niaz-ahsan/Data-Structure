1bdaec09fbebdf84cbb55297c186e6349f71b7da
niaz-ahsan <thatisniaz@gmail.com>
2021-02-23 23:50:51 -0800

    Graph: BFT is being implemented in BFS

--- Graph/BFS/main.cpp
+++ Graph/BFS/main.cpp
@@ -29,8 +29,8 @@ public:
         return found;
     }
 
-    void BFT(int start) {
-
+    void BFT() {
+        
     }
 private:
     int _limit;
@@ -50,13 +50,15 @@ private:
 };
 
 int main() {
-    Graph g(4);
+    Graph g(6);
     g.add_edge(0,1);
     g.add_edge(0,2);
-    g.add_edge(1,2);
+    g.add_edge(1,3);
     g.add_edge(2,3);
-    g.add_edge(2,0);
-    g.add_edge(3,3);
+    g.add_edge(2,4);
+    g.add_edge(2,5);
+    g.add_edge(5,0);
 
-    std::cout << g.BFS(5) << std::endl;
+    std::cout << g.BFS(14) << std::endl;
+    g.BFT();
 }
